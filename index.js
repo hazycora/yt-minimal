2232d29eb48a0bb81097d32da154817de7358dda
Hazy Cora <hazycora@gmail.com>
2021-02-07 09:06:47 -0600

    make simple video player

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const http = require('http')
 const url = require('url')
 const ytdl = require('ytdl-core')
+const fs = require('fs')
+const pug = require('pug')
 
 http.createServer(function (req, res) {
-  const queryObject = url.parse(req.url,true).query
-  console.log("Request for: \n"+JSON.stringify(queryObject, null, 2))
+  if(req.url=="/style.css"||req.url=="/style.css/") {
+    fs.readFile(__dirname+"/style.css", function (err,data) {
+      if (err) {
+        res.writeHead(404)
+        res.end(JSON.stringify(err))
+        return
+      }
+      res.writeHead(200)
+      res.end(data)
+    });
+  }else {
+    const queryObject = url.parse(req.url,true).query
+    console.log("Request for: \n"+JSON.stringify(queryObject, null, 2))
 
-  if(queryObject.v==undefined) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('UNFINISHED!!')
-  }else{
-    ytdl.getInfo(queryObject.v).then(info => {
-      const json = JSON.stringify(info, null, 2)
-      console.log(json)
+    if(queryObject.v==undefined) {
       res.writeHead(200, {'Content-Type': 'text/html'})
-      res.end(info.videoDetails.title)
-    })
+      res.end('UNFINISHED!!')
+    }else{
+      ytdl.getInfo(queryObject.v).then(info => {
+        const json = JSON.stringify(info, null, 2)
+
+        let format = ytdl.chooseFormat(info.formats, { quality: '18' })
+        console.log('Format found!')
+
+        res.writeHead(200, {'Content-Type': 'text/html'})
+
+        let html = pug.renderFile('player.pug', {
+          format: format,
+          info: info,
+          query: queryObject
+        })
+        res.end(html)
+      })
+    }
   }
 }).listen(3002)

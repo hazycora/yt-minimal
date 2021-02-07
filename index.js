d65469eb858f8aa71603f844d1d92987a8de445e
Hazy Cora <hazycora@gmail.com>
2021-02-07 07:54:48 -0600

    Added http server

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http")
+const host = 'localhost'
+const port = 3002
+
+const requestListener = function (req, res) {
+    res.writeHead(200)
+    res.end("Site initialized!")
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`)
+})
